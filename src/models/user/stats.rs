7772c2838df11838f41fcb7ef39e56c6490d0f85
commit2main <229308316+commit2main@users.noreply.github.com>
2026-07-04 06:01:45 +0000

    resolve feedback

--- src/models/user/stats.rs
+++ src/models/user/stats.rs
@@ -8,7 +8,7 @@ use crate::models::error::AppError;
 #[derive(Serialize)]
 pub struct UserStats {
     pub admin: u64,
-    pub finance: u64,
+    pub accountant: u64,
     pub staff: u64,
 }
 
@@ -27,11 +27,11 @@ impl UserStats {
             .filter(user_entity::Column::Disabled.eq(false))
             .count(db);
 
-        let (admin, finance, staff) = futures::try_join!(admin_fut, finance_fut, staff_fut)?;
+        let (admin, accountant, staff) = futures::try_join!(admin_fut, finance_fut, staff_fut)?;
 
         Ok(UserStats {
             admin,
-            finance,
+            accountant,
             staff,
         })
     }
